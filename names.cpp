5954dba5e59cbe5a11336fa15d9f8a67d6076ce2
ExistentialLamp <89541058+ExistentialLamp@users.noreply.github.com>
2021-09-20 10:38:53 -0800

    printNames(): only prints names doesn't do any transformation

--- names.cpp
+++ names.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -17,13 +18,13 @@ void getNames(vector<string>  &  reference) {
     for(int i=0; i<10; i++){
         string name;
         cout << "Enter a name: ";
-        cin >> name;
+        std::getline(cin, name);
         reference.push_back(name);
     }
 }
 
 bool doesNameExist(const string  &  testName, const vector<string>  &  reference){
-    for(int i=0; i<10; i++){
+    for(int i=0; i<reference.size(); i++){
         if(reference.at(i) == testName){
             return true;
         }
@@ -31,9 +32,16 @@ bool doesNameExist(const string  &  testName, const vector<string>  &  reference
     return false;
 }
 
+void printNames(const vector<string>  &  reference){
+    for(int i=0; i<reference.size(); i++){
+        cout << reference.at(i) << endl;
+    }
+}
+
 int main() {
     vector<string> names;
     getNames(names);
-    cout << doesNameExist("name", names);
+    cout << doesNameExist("name", names) << endl;
+    printNames(names);
     return 0;
 }
